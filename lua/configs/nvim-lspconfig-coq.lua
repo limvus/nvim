09a9962ed807c1f5c7ef15fece98c3fbe3463dcb
Sudip Limbu <4slimbu@gmail.com>
2021-12-22 09:49:22 +0545

    Remove keymap for nvim-lspconfig

--- lua/configs/nvim-lspconfig-coq.lua
+++ lua/configs/nvim-lspconfig-coq.lua
@@ -1,49 +1,9 @@
 local lspconfig = require "lspconfig"
-local coq = require "coq"
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-  -- Mappings.
-  local opts = {noremap = true}
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-end
 
 ----------------
 -- Html Server
 ----------------
-lspconfig.html.setup(coq.lsp_ensure_capabilities({capabilities = capabilities, on_attach = on_attach}))
+lspconfig.html.setup({})
 
 -----------------
 -- Lua Server
@@ -69,10 +29,9 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 lspconfig.sumneko_lua.setup(
-  coq.lsp_ensure_capabilities(
+
     {
       cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-      on_attach = on_attach,
       settings = {
         Lua = {
           runtime = {
@@ -96,15 +55,14 @@ lspconfig.sumneko_lua.setup(
         }
       }
     }
-  )
 )
 
 -----------------------
 -- Javascript server
 ----------------------
-lspconfig.tsserver.setup(coq.lsp_ensure_capabilities({on_attach = on_attach}))
+lspconfig.tsserver.setup({})
 
 -----------------------
 -- PHP server
 -----------------------
-lspconfig.intelephense.setup(coq.lsp_ensure_capabilities({on_attach = on_attach}))
+lspconfig.intelephense.setup({})
